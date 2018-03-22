55a56c75d73bd3fa022cb57868ff7aec948e5cba
Fanghao(Gabriel) Song <fanghaos2014@gmail.com>
2018-03-21 21:31:59 -0400

    Add files via upload

---
+++ SumDigit.java
@@ -0,0 +1,29 @@
+
+import list.*;
+
+public class SumDigit {
+
+	public SList sumList (SList inp1, SList inp2){
+		if((inp1 == null || inp2 == null){
+			return null;
+		} else {
+			SListNode front1 = (SListNode) inp1.front();
+			SListNode front2 = (SListNode) inp2.front();
+			
+		}
+		return null;
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SList inpp1 = new SList();
+		inpp1.
+		
+
+	}
+
+}
